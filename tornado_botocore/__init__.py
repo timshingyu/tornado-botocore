c017bc20dd8f65f49c9aff3ac828544a3bfd25e8
EC2 Default User <ec2-user@ip-172-31-8-112.ap-southeast-1.compute.internal>
2018-05-04 07:48:33 +0000

    Updated config

--- tornado_botocore/__init__.py
+++ tornado_botocore/__init__.py
@@ -3,7 +3,7 @@ import logging
 __version__ = '1.3.2'
 
 try:
-    from base import Botocore
+    from app.base import Botocore
 except ImportError:
     logging.warning('It looks like some requirements are missing.')
 
